33f6ac4c1b540bbf2b6e76ec76a5a2cb39309e43
ValentinLagues <valentin.lagues@gmail.com>
2022-01-14 01:36:35 +0100

    gold font

--- src/components/music.jsx
+++ src/components/music.jsx
@@ -7,16 +7,14 @@ const Music = () => {
   const { musicPlayer } = useContext(MusicContext);
   return (
     <div className="music-component">
-      {musicPlayer && (
-        <ReactPlayer
-          url={playlist[Math.round(Math.random() * 6)].src}
-          width="0"
-          height="0"
-          playing={true}
-          loop={true}
-          volume={0.3}
-        />
-      )}
+      <ReactPlayer
+        url={playlist[Math.round(Math.random() * 6)].src}
+        width="0"
+        height="0"
+        playing={musicPlayer}
+        loop={true}
+        volume={0.3}
+      />
     </div>
   );
 };
